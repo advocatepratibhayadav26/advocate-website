a4f5ec44a33019a10c162517e4a6df4d3a519616
advocatepratibhayadav26 <advocatepratibhayadav26@gmail.com>
2026-07-04 18:27:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-const form = document.querySelector("form");
+    emailjs.init("usQhC7MdvtgvFHMtx");
 
-form.addEventListener("submit", function(e){
+    const form = document.getElementById("appointmentForm");
 
-e.preventDefault();
+    form.addEventListener("submit", function (e) {
 
-alert("Thank you for contacting Advocate Pratibha Yadav. We will contact you shortly.");
+        e.preventDefault();
 
-form.reset();
+        const fullName = document.getElementById("fullName").value;
+        const mobile = document.getElementById("mobile").value;
+        const issue = document.getElementById("issue").value;
 
-});
+        emailjs.send("service_kit0x37", "template_cmg5qlq", {
+            fullName: fullName,
+            mobile: mobile,
+            issue: issue
+        }).then(function () {
+
+            const msg =
+`New Appointment
+
+Name: ${fullName}
+Mobile: ${mobile}
+Issue: ${issue}`;
+
+            window.open("https://wa.me/919454337340?text=" + encodeURIComponent(msg), "_blank");
+
+            alert("Appointment Submitted Successfully!");
+
+            form.reset();
+
+        }).catch(function (error) {
+            alert("Error: " + JSON.stringify(error));
+        });
+
+    });
 
 });
